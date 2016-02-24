a6e24a761b8b4522f88b166cd34ce9156813b88e
Brandon Williams <bmw014@eng.ucsd.edu>
2016-02-23 17:59:52 -0800

    lame update

--- viz/js/viz.js
+++ viz/js/viz.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   // Constants
   //
+  //
   var margin = {top: 20, right: 20, bottom: 30, left: 40};
   var width = $("#content").width() - margin.right - margin.left;
   var height = 600;
